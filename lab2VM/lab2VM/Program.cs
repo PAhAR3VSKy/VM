0f5722eefced5a1b2ebe91bd2c9da81678c0f91f
paharevsky <jo.2015@yandex.ru>
2020-05-07 20:12:24 +0700

    доделать итерации

--- lab2VM/lab2VM/Program.cs
+++ lab2VM/lab2VM/Program.cs
@@ -11,117 +11,136 @@ namespace lab2VM
     class Program
     {
         
-        public static int Size = 3;
+        //static double convergence(double[,] A)
+        //{
+        //    double count = 0;
+        //    double tempValue = 0;
+        //    for (int i = 0; i < Size; i++)
+        //    {
+        //        for (int j = 0; j < Size; j++)
+        //        {
+        //            tempValue += Math.Abs(A[i, j]);
+        //        }
+        //        if (Math.Abs(A[i, i]) > tempValue - Math.Abs(A[i, i]))
+        //            count++;
+        //    }
+        //    return count;
+        //}
 
-        static double convergence(double[,] A)
-        {
-            double count = 0;
-            double tempValue = 0;
-            for (int i = 0; i < Size; i++)
-            {
-                for (int j = 0; j < Size; j++)
-                {
-                    tempValue += Math.Abs(A[i, j]);
-                }
-                if (Math.Abs(A[i, i]) > tempValue - Math.Abs(A[i, i]))
-                    count++;
-            }
-            return count;
-        }
+        //static void Iter_Form(double[,]A, double[]b, ref double[,] C, ref double[]d) //получение итерационной формы системы
+        //{
+        //    for (int i = 0; i < Size; i++)
+        //    {
+        //        for (int j = 0; j < Size; j++)
+        //        {
+        //            if (i == j)
+        //                C[i, j] = 0;
+        //            else
+        //                C[i, j] = -A[i, j] / A[i, i];
+        //        }
+        //        d[i] = b[i] / A[i, i];
+        //    }
+        //}
 
-        static void Iter_Form(double[,]A, double[]b, ref double[,] C, ref double[]d) //получение итерационной формы системы
-        {
-            for (int i = 0; i < Size; i++)
-            {
-                for (int j = 0; j < Size; j++)
-                {
-                    if (i == j)
-                        C[i, j] = 0;
-                    else
-                        C[i, j] = -A[i, j] / A[i, i];
-                }
-                d[i] = b[i] / A[i, i];
-            }
-        }
+        //static bool Check_Convergence(ref double[,]C, ref double[]d)
+        //{
+        //    int summ = 0;
+        //    for (int i = 0; i < Size; i++)
+        //    {
+        //        for (int j = 0; j < Size; j++)
+        //            summ = (int)(summ + C[i, j] * C[i, j]);
+        //    }
 
-        static bool Check_Convergence(ref double[,]C, ref double[]d)
-        {
-            int summ = 0;
-            for (int i = 0; i < Size; i++)
-            {
-                for (int j = 0; j < Size; j++)
-                    summ = (int)(summ + C[i, j] * C[i, j]);
-            }
-
-            if (Math.Sqrt(Math.Abs(summ)) > 1)
-            {
-                Console.WriteLine("Данная система не удовлетворяет условию сходимости");
-                return false;
-            }
-            else
-                return true;
-        }
+        //    if (Math.Sqrt(Math.Abs(summ)) > 1)
+        //    {
+        //        Console.WriteLine("Данная система не удовлетворяет условию сходимости");
+        //        return false;
+        //    }
+        //    else
+        //        return true;
+        //}
 
 
 
-        static double[] Simple_Iteration(ref double[,] A, ref double[] b) //метод простых итераций
-        {
-            double[,] matrixC = new double[Size, Size];
-            double[] matrixD = new double[Size];
-            double[] x0 = new double[Size];
-            double[] X = new double[Size];
-            double[] E = new double[Size];
-            double delta;
-            x0 = b;
+        //static double[] Simple_Iteration(ref double[,] A, ref double[] b) //метод простых итераций
+        //{
+        //    double[,] matrixC = new double[Size, Size];
+        //    double[] matrixD = new double[Size];
+        //    double[] x0 = new double[Size];
+        //    double[] X = new double[Size];
+        //    double[] E = new double[Size];
+        //    double delta;
+        //    x0 = b;
 
-            Iter_Form(A, b, ref matrixC, ref matrixD);
-            while(!(Check_Convergence(ref matrixC, ref matrixD)))
-            {
-                // перестановка строк
-            }
-            do
-            {
-                for (int i = 0; i < Size; i++)
-                {
-                    X[i] = 0;
-                    for (int j = 0; j < Size; j++)
-                        X[i] = X[i] + matrixC[i, j] * x0[j];
-                    X[i] = X[i] + matrixD[i];
-                    E[i] = Math.Abs(X[i] - x0[i]);
-                }
-                delta = E[0];
+        //    Iter_Form(A, b, ref matrixC, ref matrixD);
+        //    while(!(Check_Convergence(ref matrixC, ref matrixD)))
+        //    {
+        //        // перестановка строк
+        //    }
+        //    do
+        //    {
+        //        for (int i = 0; i < Size; i++)
+        //        {
+        //            X[i] = 0;
+        //            for (int j = 0; j < Size; j++)
+        //                X[i] = X[i] + matrixC[i, j] * x0[j];
+        //            X[i] = X[i] + matrixD[i];
+        //            E[i] = Math.Abs(X[i] - x0[i]);
+        //        }
+        //        delta = E[0];
 
-                for (int i = 0; i < Size; i++)
-                {
-                    if (delta < E[i])
-                        delta = E[i];
-                }
-                x0 = X;
-            } while (delta <= 0.00001);
+        //        for (int i = 0; i < Size; i++)
+        //        {
+        //            if (delta < E[i])
+        //                delta = E[i];
+        //        }
+        //        x0 = X;
+        //    } while (delta <= 0.00001);
 
-            return X;
-        }
+        //    return X;
+        //}
 
     
         static void Main(string[] args)
         {
             SLAU matrix = new SLAU();
+            int n;
+            int choice;
+            Console.WriteLine("Введите количество уравнений: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            double[,] matrixA = new double[n, n];
+            double[] matrixB = new double[n];
+            double[] x = new double[n];
 
-            double[,] matrixA = new double[Size, Size];
-            double[] matrixB = new double[Size];
-            double[] matrixCout;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.WriteLine("A[" + i + "][" + j + "] = ");
+                    matrixA[i, j] = Convert.ToDouble(Console.ReadLine());
+                }
+            }
+            Console.WriteLine("__________________________________________");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("b[" + i + "] = ");
+                matrixB[i] = Convert.ToDouble(Console.ReadLine());
+            }
 
-            matrixA[0, 0] = 3;  matrixA[0, 1] = 1;  matrixA[0, 2] = 10;
-            matrixA[1, 0] = 14; matrixA[1, 1] = 2;  matrixA[1, 2] = 3;
-            matrixA[2, 0] = 2;  matrixA[2, 1] = 12; matrixA[2, 2] = 3;
+            Console.WriteLine("1. Метод Гаусса с выбором главного элемента по строке;\n" +
+                              "2. Метод простой итерации.");
+            choice = Convert.ToInt32(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    x = matrix.Gauss_Method(matrixA, matrixB, n);
+                    break;
+                default:
+                    break;
+            }
 
-            matrixB[0] = 18;    matrixB[1] = 35;    matrixB[2] = 31;
-
-            matrixCout = matrix.Gauss_Method(matrixA, matrixB);
-            for (int i = 0; i < Size; i++)
-                matrixCout[i] = Math.Round(matrixCout[i], 0, MidpointRounding.ToEven);
-
-            Console.WriteLine("{0}", string.Join("\n", matrixCout));
+            for (int i = 0; i < n; i++)
+                Console.WriteLine("x[" + i + "] = " + x[i]);
         }
     }
 }
--- lab2VM/lab2VM/SLAU.cs
+++ lab2VM/lab2VM/SLAU.cs
@@ -4,22 +4,32 @@ namespace lab2VM
 
     class SLAU
     {
-        private static int Size = 3;
+        
+        public void Print(double[,]A, double[]b, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("|");
+                for (int j = 0; j < n; j++)
+                    Console.Write(A[i, j]);
+                Console.Write("|" + b[i] + "|");
 
-        public double[] Gauss_Method(double[,] A, double[] b)
+            }
+        }
+
+        public double[] Gauss_Method(double[,] A, double[] b, int n)
         {
             int k = 0,
                 index = 0;
-            double max = 0;
-            double[] x = new double[Size];
-            double[] tempMass = new double[2];
+            double max;
+            double[] x = new double[n];
 
-            while (k < Size)
+            while (k < n)
             {
                 max = Math.Abs(A[0, 0]);
-                for (int i = 0; i < Size; i++)  // Поиск строки с максимальным элементом
+                for (int i = 0; i < n; i++)  // Поиск строки с максимальным элементом
                 {
-                    for (int j = 0; j < Size; j++)
+                    for (int j = 0; j < n; j++)
                     {
                         if (Math.Abs(A[i, j]) > max)
                         {
@@ -36,32 +46,43 @@ namespace lab2VM
                     Console.WriteLine(index + " матрицы A\n");
                     return null;
                 }
-                for (int i = 0; i < Size; i++)
+
+                for (int i = 0; i < n; i++)
                 {
                     double temp1 = A[k, i];
                     A[k, i] = A[index, i];
                     A[index, i] = temp1;
                 }
+
                 double temp2 = b[k];
                 b[k] = b[index];
                 b[index] = temp2;
+
                 // Нормализация уравнений
-                for (int i = k; i < Size; i++)
+                for (int i = k; i < n; i++)
                 {
                     double temp = A[i, k];
-                    if (Math.Abs(temp) < 0) continue;   // для нулевого коэффициента пропустить
-                    for (int j = 0; j < Size; j++)
+
+                    if (Math.Abs(temp) < 0)
+                        continue;   // для нулевого коэффициента пропустить
+
+                    for (int j = 0; j < n; j++)
                         A[i, j] = A[i, j] / temp;
+
                     b[i] = b[i] / temp;
-                    if (i == k) continue;   // уравнение не вычитать само из себя
-                    for (int j = 0; j < Size; j++)
+
+                    if (i == k)
+                        continue;   // уравнение не вычитать само из себя
+
+                    for (int j = 0; j < n; j++)
                         A[i, j] = A[i, j] - A[k, j];
+
                     b[i] = b[i] - b[k];
                 }
                 k++;
             }
             // обратная подстановка
-            for (k = Size - 1; k >= 0; k--)
+            for (k = n - 1; k >= 0; k--)
             {
                 x[k] = b[k];
                 for (int i = 0; i < k; i++)
